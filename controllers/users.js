adf68c932f92639cbdb81ad35a294887b9872127
Edgard Carrillo <ea_carrillo@hotmail.com>
2023-10-16 13:30:23 -0500

    fazt

--- controllers/users.js
+++ controllers/users.js
@@ -1,78 +1,84 @@
-const usersmodel = require('../models/users')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const usersmodel = require("../models/users");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const create = async (req, res) => {
+  console.log("ejecutando registro de usuarios");
 
-    console.log("ejecutando registro de usuarios");
+  try {
+    const passwordhash = await bcrypt.hash(req.body.contrasena, 10);
+    req.body.contrasena = passwordhash;
+    const userregister = new usersmodel(req.body);
+    const UserSaved = await userregister.save();
 
-    try {
-        const passwordhash = await bcrypt.hash(req.body.contrasena, 10)
-        req.body.contrasena = passwordhash;
-        const userregister = new usersmodel(req.body);
-        const UserSaved = await userregister.save();
-
-
-        jwt.sign({ id: UserSaved._id }, process.env.SECRET_TOKEN, { expiresIn: "1d" },
-            (err, token) => {
-                if (err) console.log(err);
-                res.cookie('token', token)
-                res.json({ message: "usuario creado" })
-            })
-
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-    // res.status(200).json(UserSaved)
-}
-
-
+    jwt.sign(
+      { id: UserSaved._id },
+      process.env.SECRET_TOKEN,
+      { expiresIn: "1d" },
+      (err, token) => {
+        if (err) console.log(err);
+        res.cookie("token", token);
+        res.json({ message: "usuario creado" });
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  // res.status(200).json(UserSaved)
+};
 
 const verify = async (req, res) => {
-    console.log("Ejecutando verificación de usuario");
-    console.log(req.body);
-    const datausers = await usersmodel.findOne({ correo: req.body.params.email })
+  console.log("Ejecutando verificación de usuario");
+  console.log(req.body);
+  const datausers = await usersmodel.findOne({ correo: req.body.params.email });
 
-    try {
-        if (!datausers) {
-            console.log("El usuario no existe");
-            return res.json({ message: "Usuario no encontrado" })
-        }
-
-        if(datausers.verificado===false) return res.status(400).json({message: "Usuario no validado"})
-
-        const isMatch = await bcrypt.compare(req.body.params.password, datausers.contrasena)
-
-        if (!isMatch) return res.status(400).json({ message: "Invalid credential" })
-
-
-        jwt.sign({ id: datausers._id }, process.env.SECRET_TOKEN, { expiresIn: "1d" },
-            (err, token) => {
-                if (err) console.log(err);
-                res.cookie('token', token)
-                console.log("Usuario verificado");
-                res.status(200).json({ message: "usuario verificado", data: "sigue mierda",token:token })
-            })
-
-
-
-
-    } catch (error) {
-        console.error("no se que mierda paso", error);
+  try {
+    if (!datausers) {
+      console.log("El usuario no existe");
+      return res.json({ message: "Usuario no encontrado" });
     }
-}
 
+    if (datausers.verificado === false)
+      return res.status(400).json({ message: "Usuario no validado" });
+
+    const isMatch = await bcrypt.compare(
+      req.body.params.password,
+      datausers.contrasena
+    );
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credential" });
+
+    jwt.sign(
+      { id: datausers._id },
+      process.env.SECRET_TOKEN,
+      { expiresIn: "1d" },
+      (err, token) => {
+        if (err) console.log(err);
+        res.cookie("token", token);
+        console.log("Usuario verificado");
+        res.status(200).json({
+          message: "usuario verificado",
+          data: "sigue mierda",
+          token: token,
+          user: datausers,
+        });
+      }
+    );
+  } catch (error) {
+    console.error("no se que mierda paso", error);
+  }
+};
 
 const profile = async (req, res) => {
-    console.log("ejecutando request profile");
-    //console.log(req.user);
-    const userfound = await usersmodel.findById(req.user.id)
-    console.log(userfound);
-    if(!userfound) res.status(400).json({message: "user not found"})
-    return res.json({
-        correo: userfound.correo,
-        empresa: userfound.empresa
-    })
-}
+  console.log("ejecutando request profile");
+  //console.log(req.user);
+  const userFound = await usersmodel.findById(req.user.id);
+  console.log(userFound);
+  if (!userFound) res.status(400).json({ message: "user not found" });
 
-module.exports = { create, verify, profile }
+  const { contrasena, ...user } = userFound._doc;
+  return res.json(user);
+};
+
+module.exports = { create, verify, profile };
--- models/users.js
+++ models/users.js
@@ -1,16 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
 const usersquema = mongoose.Schema(
-    {
-        correo: { type: String },
-        usuario: { type: String },
-        contrasena: { type: String },
-        empresa: { type: String },
-        celular: { type: String },
-        deleted: { type: Boolean, default: false },
-        verificado: { type: Boolean, default: false }
-    }, {
-    timestamps: true
-})
+  {
+    correo: { type: String },
+    usuario: { type: String },
+    contrasena: { type: String },
+    empresa: { type: String },
+    celular: { type: String },
+    deleted: { type: Boolean, default: false },
+    verificado: { type: Boolean, default: false },
+    rol: { type: String, default: "user" }, // user, admin,
+  },
+  {
+    timestamps: true,
+  }
+);
 //MODELO
-const users = mongoose.model('user', usersquema, 'users')
-module.exports = users
+const users = mongoose.model("user", usersquema, "users");
+module.exports = users;
